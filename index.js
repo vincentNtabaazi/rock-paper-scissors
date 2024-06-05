aa990f4e7a7510afbbad58876d1e7036447319ef
vincentNtabaazi <vincentntabaazi02@gmail.com>
2024-06-05 10:59:51 +0300

    Implementing the part that shows the track of points and resets after five rounds.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const rock = document.querySelector(".rock")
 const paper = document.querySelector(".paper")
 const scissor = document.querySelector(".scissor")
+const results = document.querySelector(".results")
+const overAllWinner = document.querySelector(".overAllWinner")
+const computerScore = document.querySelector(".computerScore")
+const playerScore = document.querySelector(".playerScore")
+
 
 rock.addEventListener("click", func1);
 paper.addEventListener("click", func1);
@@ -8,7 +13,6 @@ scissor.addEventListener("click", func1);
 
 function func1(e) {
   let param = e.target.className
-  // console.log(param)
   playGame(param)
 }
 
@@ -26,66 +30,72 @@ function getComputerChoice() {
 }
 
 function singleRound(computerSelection, playerSelection) {
-    playerSelection = playerSelection.toLowerCase()
-    let choice = ["rock", "paper", "scissor"]
-    // if (!choice.includes(playerSelection)) {
-    //   playerSelection = prompt("What's your choice?")
-    // }
-    if (computerSelection === playerSelection) {
+  playerSelection = playerSelection.toLowerCase()
+  let choice = ["rock", "paper", "scissor"]
+  if (computerSelection === playerSelection) {
+    console.log(`The computer selection: ${computerSelection}`)
+    return "It is a tie"
+  } else {
+    if (computerSelection === "rock" && playerSelection === "paper") {
       console.log(`The computer selection: ${computerSelection}`)
-      return "It is a tie"
-    } else {
-      if (computerSelection === "rock" && playerSelection === "paper") {
-        console.log(`The computer selection: ${computerSelection}`)
-        return "You Win! Paper beats Rock"
-      }
-      else if (computerSelection === "rock" && playerSelection === "scissor") {
-        console.log(`The computer selection: ${computerSelection}`)
-        return "You Lose! Rock beats scissor"
-      }
-      else if (computerSelection === "paper" && playerSelection === "scissor") {
-        console.log(`The computer selection: ${computerSelection}`)
-        return "You Win! Scissor beats paper"
-      }
-      else if (computerSelection === "paper" && playerSelection === "rock") {
-        console.log(`The computer selection: ${computerSelection}`)
-        return "You Lose! Paper beats Rock"
-      }
-      else if (computerSelection === "scissor" && playerSelection === "rock") {
-        console.log(`The computer selection: ${computerSelection}`)
-        return "You Win! Rock beats scissor"
-      }
-      else if (computerSelection === "scissor" && playerSelection === "paper") {
-        console.log(`The computer selection: ${computerSelection}`)
-        return "You Lose! Scissor beats paper"
-      }
+      return "You Win! Paper beats Rock"
     }
+    else if (computerSelection === "rock" && playerSelection === "scissor") {
+      console.log(`The computer selection: ${computerSelection}`)
+      return "You Lose! Rock beats scissor"
+    }
+    else if (computerSelection === "paper" && playerSelection === "scissor") {
+      console.log(`The computer selection: ${computerSelection}`)
+      return "You Win! Scissor beats paper"
+    }
+    else if (computerSelection === "paper" && playerSelection === "rock") {
+      console.log(`The computer selection: ${computerSelection}`)
+      return "You Lose! Paper beats Rock"
+    }
+    else if (computerSelection === "scissor" && playerSelection === "rock") {
+      console.log(`The computer selection: ${computerSelection}`)
+      return "You Win! Rock beats scissor"
+    }
+    else if (computerSelection === "scissor" && playerSelection === "paper") {
+      console.log(`The computer selection: ${computerSelection}`)
+      return "You Lose! Scissor beats paper"
+    }
+  }
 }
 
 function playGame(playerChoice) {
   let playerSelection = playerChoice
   const computerSelection = getComputerChoice()
   let result = singleRound(computerSelection, playerSelection)
-  console.log(result)
-  let statusW = "Win"
-  let statusL = "Lose"
+  results.textContent = `Result: ${result}`
   if (result !== undefined) {
-    if (result.indexOf(statusW) != -1) {
-      playerWins += 1
-    } else if (result.indexOf(statusL) != -1) {
-      computerWins += 1
+    if (result.indexOf("Win") != -1) {
+      if (typeof countPlayer === "undefined") {
+        countPlayer = 0;
+      }
+      countPlayer++
+      playerScore.textContent = `Player Score: ${countPlayer}`
+    } else if (result.indexOf("Lose") != -1) {
+      if (typeof (countComputer) === "undefined") {
+        countComputer = 0;
+      }
+      countComputer++
+      computerScore.textContent = `Computer Score: ${countComputer}`
     }
   }
+    if (countComputer === 5 || countPlayer === 5) {
+      if (countComputer === 5) {
+        countComputer = 0
+        countPlayer = 0
+        alert("The computer wins in 5 rounds.")
+      } else if (countPlayer === 5) {
+        countComputer = 0
+        countPlayer = 0
+        alert("The player wins in 5 rounds.")
+      }
 
-  if (playerWins > computerWins) {
-    console.log(`The player got ${playerWins - computerWins} more wins than the computer.`)
-  } else if (playerWins < computerWins) {
-    console.log(`The computer got ${computerWins - playerWins} more wins than the player.`)
-  } else {
-    if (playerWins == 0 && computerWins == 0) {
-      console.log("The game was not played.")
-    } else {
-      console.log("It is a tie.")
-    }
+      playerScore.textContent = `Player Score: ${countPlayer}`
+      computerScore.textContent = `Computer Score: ${countComputer}`
+      results.textContent = ""
   }
 }
